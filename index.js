d1716ad563ee30ead833f6929dd1d2ae51c95862
Emma Slight <emma.slight@icloud.com>
2020-10-27 18:52:14 +1300

    get values from inpur fields for player names

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+let player1
+let player2
+const winner = document.getElementById('winner')
+
+document.getElementById('addNames').addEventListener('click', () => {
+  event.preventDefault()
+
+  const firstName = document.getElementById('player-1').value
+  const secondName = document.getElementById('player-2').value
+  player1 = Player(firstName, 'X')
+  player2 = Player(secondName, 'O')
+
+  console.log(player1)
+  console.log(player2)
+})
+
+const Player = (name, marker) => {
+  return { name, marker }
+}
+
 const gameBoard = () => {
   console.log('gameBoard start')
 
