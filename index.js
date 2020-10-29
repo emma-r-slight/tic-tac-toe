e81be6bbdc00cb920957893945d1b75248788a36
Emma Slight <emma.slight@icloud.com>
2020-10-29 23:33:51 +1300

    refactor code

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
 let player1
 let player2
 const showWinner = document.getElementById('winner')
-const image = document.getElementById('image')
-
-const showBoard = document.getElementById('showBoard')
-
 const Player = (name, marker) => {
   return { name, marker }
 }
 
-document.getElementById('addNames').addEventListener('click', () => {
-  event.preventDefault()
+const getPlayer = (() => {
+  const image = document.getElementById('image')
+  const showBoard = document.getElementById('showBoard')
+  document.getElementById('addNames').addEventListener('click', () => {
+    event.preventDefault()
 
-  const firstName = document.getElementById('player-1').value
-  const secondName = document.getElementById('player-2').value
-  player1 = Player(firstName, 'X')
-  player2 = Player(secondName, 'O')
-
+    const firstName = document.getElementById('player-1').value
+    const secondName = document.getElementById('player-2').value
+    player1 = Player(firstName, 'X')
+    player2 = Player(secondName, 'O')
+    image.classList.remove('show')
+    image.classList.add('hide')
+    showBoard.classList.remove('hide')
+    showBoard.classList.add('show')
+  })
+  return { player1, player2 }
   console.log(player1)
   console.log(player2)
-  image.classList.remove('show')
-  image.classList.add('hide')
-  gameBoard()
-  showBoard.classList.remove('hide')
-  showBoard.classList.add('show')
-})
+})()
 
-const gameBoard = () => {
+const gameBoard = (() => {
   console.log('gameBoard start')
   const showBoard = document.getElementById('game-board')
 
@@ -35,19 +34,41 @@ const gameBoard = () => {
     cell.id = `${i}`
     cell.className = 'cell'
     showBoard.append(cell)
+    console.log(cell)
   }
-  game()
-}
+})()
 
-const game = () => {
+const game = (() => {
   let playerTurn = player1
-  document.getElementById('player-turn')
   let board = Array(9).fill('')
-  const count = 0
+  let count = 0
   let winner = null
-  const cells = Array.from(document.querySelector('.cell'))
+  const cells = Array.from(document.querySelectorAll('.cell'))
+  console.log(cells)
 
-  const checkWIn = (player) => {
+  const play = (() => {
+    cells.forEach((cell) => {
+      cell.addEventListener('click', () => {
+        const cellId = cell.id - 1
+        if (board[cellId] === '' && count < 9 && winner === null) {
+          addMark(playerTurn, cellId)
+          nextPlayer(playerTurn)
+        }
+      })
+    })
+  })()
+
+  const addMark = (Player, markCell) => {
+    board[markCell] = Player.marker
+    markCell.innerHTML = Player.marker
+    count++
+    checkWin(player)
+  }
+
+  const nextPlayer = (playerTurn) => {
+    playerTurn === player1 ? player2 : player1
+  }
+  const checkWIn = (playerWin) => {
     const player1Win = `${player1.marker} ${player1.marker} ${player1.marker}`
     const player2Win = `${player2.marker} ${player2.marker} ${player2.marker}`
 
@@ -67,9 +88,9 @@ const game = () => {
       }`
 
       if (check == player1Win) {
-        winner = player.name
+        winner = playerWin.name
       } else if (check == player2win) {
-        winner = player.name
+        winner = playerWin.name
       }
     })
     if (count === 9 && winner === null) {
@@ -80,28 +101,5 @@ const game = () => {
       showWinner.classList.add('show')
       document.getElementById('won').innerHTML = `${winner}`
     }
-
-    const addMark = (player, cell) => {
-      const cellID = cell.id - 1
-      board[cellID] = player.marker
-      cell.innerText = player.marker
-      count++
-      checkWin(player)
-    }
   }
-  const play = (() => {
-    cells.forEach((cell) => {
-      cell.addEventListener('click', () => {
-        const cellId = cell.id - 1
-        if (board[cellId] === '' && count < 9 && winner === null) {
-          addMark(playerTurn, cell)
-          if (playerTurn === player1) {
-            playerTurn = player2
-          } else if (playerTurn === player2) {
-            playerTurn = player1
-          }
-        }
-      })
-    })
-  })()
-}
+})()
