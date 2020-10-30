2702e99b90beb7f48d1388cc22e84ecaac54b555
Emma Slight <emma.slight@icloud.com>
2020-10-31 02:27:23 +1300

    refactor functions to render board and get names from inputs

--- index2.js
+++ index2.js
@@ -1,97 +1,92 @@
-const _playerCreate = (name, mark) => {
-  return { name, mark }
-}
+const GameBoard = (() => {
+  const boardArray = new Array(9).fill('X')
 
-const boardFunctions = (() => {
-  const playerOne = _playerCreate('playerOne', 'X')
-  const playerTwo = _playerCreate('playerTwo', 'O')
+  const _allEqual = (a, b, c) => {
+    return a === b && a === c && a !== ''
+  }
 
-  const showBoard = document.getElementById('showBoard')
-  const image = document.getElementById('image')
-  const getBoard = document.getElementById('game-board')
-  const showWinner = document.getElementById('winner')
-  const cells = document.querySelectorAll('.cell')
-  const startButton = document.getElementById('startGame')
-  const firstName = document.getElementById('player-1')
-  const secondName = document.getElementById('player-2')
+  const checkWIn = (board) => {
+    let winner = null
+    const check = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+    for (let i = 0; i < check.length; i++) {
+      if (_allEqual(board[check[i][0]], board[check[i][1]], board[check[i][2]]))
+        winner = board[check[i][0]]
+    }
+  }
 
-  const clickListeners = (() => {
-    const start = () => startButton.addEventListener('click', startGame)
+  return { checkWIn, boardArray }
+})()
 
-    const checkCells = () =>
-      cells.forEach((cell) => {
-        cell.addEventListener('click', validateCell)
-      })
-    return { start }
-  })()
+const DisplayController = (() => {
+  const Player = (name, symbol) => {
+    return { name, symbol }
+  }
 
   const hideImage = () => {
+    const image = document.getElementById('image')
     image.classList.remove('show')
     image.classList.add('hide')
   }
 
+  const validateCell = (event) => {
+    console.log('click')
+  }
+
   const revealBoard = () => {
+    const showBoard = document.getElementById('showBoard')
     showBoard.classList.remove('hide')
     showBoard.classList.add('show')
   }
 
   const renderBoard = () => {
+    const getBoard = document.getElementById('game-board')
+    const gameArray = GameBoard.boardArray
+    console.log(gameArray)
     for (let i = 0; i < 9; i++) {
       const cell = document.createElement('div')
-      console.log(cell)
       cell.id = `${i}`
-      cell.className = 'cell'
+      cell.className = 'cell paper-btn'
+      cell.innerHTML = `${gameArray[i]}`
+      cell.addEventListener('click', validateCell)
       getBoard.append(cell)
-      revealBoard()
+      console.log(cell)
     }
+    revealBoard()
   }
+  const firstName = document.getElementById('player-1')
+  const secondName = document.getElementById('player-2')
+  const playerOne = Player(firstName.value, 'X')
+  const playerTwo = Player(secondName.value, 'O')
+  const formNames = document.getElementById('nameForm')
+
+  const clickListeners = (() => {
+    const startButton = document.getElementById('startGame')
+    const start = () => startButton.addEventListener('click', startGame)
+    return { start }
+  })()
 
   const startGame = () => {
     playerOne.name = firstName.value
     playerTwo.name = secondName.value
     hideImage()
     renderBoard()
-    console.log(playerOne)
-    console.log(playerTwo)
-  }
+    formNames.style.display = 'none'
 
-  const validateCell = (event) => {
-    if (event.target.innerHTML === '') {
-      addMark(event)
-    }
+    const count = 0
+    let getWinner = GameBoard.checkWIn(GameBoard.boardArray)
+    const cells = document.getElementsByClassName('cell')
+
+    return playerOne, playerTwo
   }
 
   clickListeners.start()
-  return
 })()
-
-const boardArray = new Array(9).fill('')
-const checkWIn = (player) => {
-  const win = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ]
-  win.forEach((winCase) => {
-    let check = `${board[winCase[0]]} ${board[winCase[1]]} ${board[winCase[2]]}`
-
-    if (check == player1Win) {
-      winner = player.name
-    } else if (check == player2win) {
-      winner = player.name
-    }
-  })
-  if (count === 9 && winner === null) {
-    winner = 'Tied'
-  }
-  if (winner !== null) {
-    showWinner.classList.remove('hide')
-    showWinner.classList.add('show')
-    document.getElementById('won').innerHTML = `${winner}`
-  }
-}
